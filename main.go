c07db4de9c4bf08357bd405bd295a32318fd4b7a
Prashant Gupta <prashantgupta@us.ibm.com>
2018-07-11 12:03:45 -0700

    multi processing

--- main.go
+++ main.go
@@ -9,7 +9,13 @@ import (
 	"github.com/emersion/go-imap/client"
 )
 
-func main() {
+type DetailedError struct {
+	errVal error
+	seqset string
+}
+
+func getEmails(messages chan *imap.Message, wg *sync.WaitGroup, from, to int) {
+
 	log.Println("Connecting to server...")
 
 	// Connect to server
@@ -22,6 +28,7 @@ func main() {
 	// Don't forget to logout
 	defer c.Logout()
 	defer log.Println("Logging out")
+	defer wg.Done()
 
 	// Login
 	if err := c.Login("stuffsom", "some_password1"); err != nil {
@@ -29,51 +36,28 @@ func main() {
 	}
 	log.Println("Logged in")
 
-	// List mailboxes
-	// mailboxes := make(chan *imap.MailboxInfo, 10)
-	// //done := make(chan error, 1)
-
-	// err = c.List("", "*", mailboxes)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// // go func() {
-	// // 	done <- c.List("", "*", mailboxes)
-	// // }()
-
-	// log.Println("Mailboxes:")
-	// for m := range mailboxes {
-	// 	log.Println("* " + m.Name)
-	// }
-
-	// if err := <-done; err != nil {
-	// 	log.Fatal(err)
-	// }
-
 	// Select INBOX
-	mbox, err := c.Select("INBOX", false)
+	_, err = c.Select("INBOX", false)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//log.Println("Flags for INBOX:", mbox.Flags)
-	log.Println("number of messages : ", mbox.Messages)
+	//log.Println("number of messages : ", mbox.Messages)
 
-	// //fetch all messages
-	// from := uint32(1)
-	// //to := mbox.Messages
-	// to := uint32(1)
-	// seqset := new(imap.SeqSet)
-	// seqset.AddRange(from, to)
-	messages := make(chan *imap.Message)
-	// errChan := make(chan error)
-	MessageMap := make(map[string]int)
-	var wg sync.WaitGroup
+	seqset := new(imap.SeqSet)
+	seqset.AddRange(uint32(from), uint32(to))
 
-	type DetailedError struct {
-		errVal error
-		seqset string
+	log.Println("executing for seqset", seqset.String())
+	err = c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
+	if err != nil {
+		log.Println(err)
 	}
+}
+
+func main() {
+
+	MessageMap := make(map[string]int)
+
+	var wg sync.WaitGroup
 
 	start := time.Now()
 	defer func() {
@@ -82,83 +66,32 @@ func main() {
 		log.Printf("Time taken %s", elapsed)
 	}()
 
-	const NumEmailsPerWorker = 1000
-	for i := uint32(1); i <= mbox.Messages; i = i + NumEmailsPerWorker {
+	const NumEmails = 50
+	//messages := make(chan *imap.Message)
+	for i := 1; i <= 200; i = i + NumEmails {
 		wg.Add(1)
 		m := make(chan *imap.Message)
-		e := make(chan DetailedError)
-		seqset := new(imap.SeqSet)
-		seqset.AddRange(i, i+(NumEmailsPerWorker-1))
-		//seqset.AddRange(i, mbox.Messages)
+		go getEmails(m, &wg, i, i+(NumEmails-1))
 
-		go func(seqset *imap.SeqSet, m chan *imap.Message, e chan DetailedError) {
-			defer wg.Done()
-			defer close(e)
-
-			log.Println("executing for seqset", seqset.String())
-			errVal := c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, m)
-			e <- DetailedError{
-				errVal: errVal,
-				seqset: seqset.String(),
-			}
-		}(seqset, m, e)
-
-		go func(m chan *imap.Message, e chan DetailedError) {
+		go func(m chan *imap.Message) {
 			for msg := range m {
-				//log.Println("message is ", msg)
-				messages <- msg
+				MessageMap[msg.Envelope.Subject] = 1
+				//messages <- msg
+				//log.Println("* Message read : " + msg.Envelope.Subject)
+				//wg.Done()
 			}
-			for DetailedError := range e {
-				if DetailedError.errVal != nil {
-					log.Println("fatal error for seqset", DetailedError.seqset)
-					// log.Fatal(err)
-				}
-			}
-
-		}(m, e)
+		}(m)
 	}
-	// go func() {
-	// 	errChan <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
-	// }()
-
-	// go func() {
-	// 	for msg := range messages {
-	// 		MessageMap[msg.Envelope.Subject] = 1
-	// 		//log.Println("* Message read : " + msg.Envelope.Subject)
-	// 		//wg.Done()
-	// 	}
-	// }()
-
-	wg.Wait()
-
-	// if err := <-errChan; err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// // Get the last 4 messages
-	// from := uint32(1)
-	// to := mbox.Messages
-	// if mbox.Messages > 3 {
-	// 	// We're using unsigned integers here, only substract if the result is > 0
-	// 	from = mbox.Messages - 3
-	// }
-	// seqset := new(imap.SeqSet)
-	// seqset.AddRange(from, to)
-
-	// messages := make(chan *imap.Message, 10)
-	// done := make(chan error, 1)
-	// go func() {
-	// 	done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
-	// }()
-
-	// log.Println("Last 4 messages:")
 	// for msg := range messages {
-	// 	log.Println("* " + msg.Envelope.Subject)
+	// 	MessageMap[msg.Envelope.Subject] = 1
+	// 	//log.Println("* Message read : " + msg.Envelope.Subject)
+	// 	//wg.Done()
 	// }
+	wg.Wait()
+	go func() {
 
-	// if err := <-done; err != nil {
-	// 	log.Fatal(err)
-	// }
+		//close(messages)
+	}()
 
 	log.Println("Done!")
 }
