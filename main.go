b7d60e789f20c26f68d355516c9623ec611d22f3
Prashant Gupta <prashantgupta@us.ibm.com>
2018-07-11 16:12:47 -0700

    refactoring

--- main.go
+++ main.go
@@ -9,21 +9,16 @@ import (
 	"github.com/emersion/go-imap/client"
 )
 
-type DetailedError struct {
-	errVal error
-	seqset string
-}
-
 func getEmails(messages chan *imap.Message, wg *sync.WaitGroup, from, to int) {
 
-	log.Println("Connecting to server...")
+	//log.Println("Connecting to server...")
 
 	// Connect to server
 	c, err := client.DialTLS("imap.yandex.com:993", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Connected")
+	//log.Println("Connected to server")
 
 	// Don't forget to logout
 	defer c.Logout()
@@ -55,43 +50,30 @@ func getEmails(messages chan *imap.Message, wg *sync.WaitGroup, from, to int) {
 
 func main() {
 
-	MessageMap := make(map[string]int)
-
+	var MessageMap sync.Map
 	var wg sync.WaitGroup
+	NumEmailsRead := 0
 
 	start := time.Now()
 	defer func() {
-		log.Printf("Read %v messages", len(MessageMap))
+		log.Printf("Read %v messages", NumEmailsRead)
 		elapsed := time.Since(start)
 		log.Printf("Time taken %s", elapsed)
 	}()
 
-	const NumEmails = 50
-	//messages := make(chan *imap.Message)
-	for i := 1; i <= 200; i = i + NumEmails {
+	const NumEmails = 500
+	for i := 1; i <= 2000; i = i + NumEmails {
 		wg.Add(1)
 		m := make(chan *imap.Message)
 		go getEmails(m, &wg, i, i+(NumEmails-1))
 
 		go func(m chan *imap.Message) {
 			for msg := range m {
-				MessageMap[msg.Envelope.Subject] = 1
-				//messages <- msg
-				//log.Println("* Message read : " + msg.Envelope.Subject)
-				//wg.Done()
+				MessageMap.Store(msg.Envelope.Subject, 1)
+				NumEmailsRead++
 			}
 		}(m)
 	}
-	// for msg := range messages {
-	// 	MessageMap[msg.Envelope.Subject] = 1
-	// 	//log.Println("* Message read : " + msg.Envelope.Subject)
-	// 	//wg.Done()
-	// }
+
 	wg.Wait()
-	go func() {
-
-		//close(messages)
-	}()
-
-	log.Println("Done!")
 }
